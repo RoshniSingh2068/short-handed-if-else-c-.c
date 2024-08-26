6b9b9469596f0632ad4d9e39365143ac5026f232
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:56:59 +0530

    Create short handed if else c++.c

---
+++ short handed if else c++.c
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int time = 22;
+    if (time<10){
+        cout << "good day";
+
+    } else if (time<20){
+       cout<< "good afternoon";
+    } else {
+        cout << "good night";
+
+    }
+
+    return 0;
+}
